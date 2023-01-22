72f372e758ed4d86d72ad830deb3f2278c272f00
Anelya <anel.bulatova7@gmail.com>
2023-01-22 21:56:05 +0300

    brackets

---
+++ main.js
@@ -0,0 +1,30 @@
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = 7;
+b = a;
+var a = 9;
+var myFirstName = "Anelya";
+var myLastName = "Bulatova";
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+// Variable declarations
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+// Variable assignments
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+titleCaseOver = 9000;
+// read carefully;
+
+var firstLetterOfLastName = “lastName”;
+
+var firstLetterOfLastName = lastName[0];
